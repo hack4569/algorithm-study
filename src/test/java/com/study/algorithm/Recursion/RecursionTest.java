c37fcf67eb4473003c0dfcc1a20f53606bf452a6
hack4569@naver.com <hack4569@naver.com>
2023-08-01 06:59:15 +0900

    재귀공부 - a의 b승

---
+++ src/test/java/com/study/algorithm/Recursion/RecursionTest.java
@@ -0,0 +1,19 @@
+package com.study.algorithm.Recursion;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RecursionTest {
+    @Test
+    public void test1Action(){
+        Assertions.assertEquals(9, test1(3,2));
+    }
+    public int test1(int a, int b){
+        if(a==0 || a==1 || b==0){
+            return 1;
+        }
+        return a * test1(a, b - 1);
+    }
+}
