127071ce02ea951db578f4f5c77a9b531d5ae9b2
hack4569@naver.com <hack4569@naver.com>
2024-04-26 23:45:40 +0900

    재귀

--- src/test/java/com/study/algorithm/RecursionTest.java
+++ src/test/java/com/study/algorithm/RecursionTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootTest
 public class RecursionTest {
     @Test
@@ -15,5 +18,27 @@ public class RecursionTest {
             return 1;
         }
         return a * test1(a, b - 1);
+    }
+
+    @Test
+    public void test2Action() {
+        String word = "AAAAE";
+        int answer = 6;
+
+        Assertions.assertEquals(answer, test2("").indexOf(word));
+    }
+
+    private static final char[] CHARS = "AEIOU".toCharArray();
+
+    private List<String> test2(String word) {
+        List<String> words = new ArrayList<>();
+        words.add(word);
+
+        if (word.length() == 5) return words;
+
+        for (char c : CHARS) {
+            words.addAll(test2(word + c));
+        }
+        return words;
     }
 }
